88bf6c6909b2e497cc853810956eb3764bd049c0
Eyyupcan Isler <116599822+eyyupcanisler@users.noreply.github.com>
2023-11-30 18:53:27 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
-let storedPoints; // Declare storedPoints globally
-
 document.addEventListener('DOMContentLoaded', function () {
     const pointCounter = document.getElementById('pointCounter');
 
     // Retrieve the user's points from localStorage
-    storedPoints = localStorage.getItem('userPoints');
+    const storedPoints = localStorage.getItem('userPoints');
 
     // If points are found in localStorage, update the display
     if (storedPoints !== null) {
         pointCounter.innerText = 'Points: ' + storedPoints;
     }
-
-    // Check if storedPoints is equal to '100'
-    if (storedPoints === '100') {
-        alert("You won the game:)");
-        location.reload(); // Refresh the page
-    }
 });
 
-window.addEventListener('beforeunload', function () {
-    // Check if storedPoints is greater than or equal to 100
-    if (parseInt(storedPoints) >= 100) {
-        storedPoints = 0;
-        localStorage.setItem('userPoints', storedPoints);
-    }
-    // Reset points only when the browser is closed
-});
+//window.addEventListener('beforeunload', function () {
+//Reset points only when the browser is closed
+//  localStorage.setItem('userPoints', 0);
+//});
+
 
 function gogame1() {
     window.location.href = 'game1.html';
 }
-
 function gogame2() {
     window.location.href = 'game2.html';
 }
-
 function gogame3() {
     window.location.href = 'game3.html';
 }
-
 function gogame4() {
     window.location.href = 'game4.html';
 }
